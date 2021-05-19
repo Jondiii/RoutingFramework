72e4548dedad8fef7c8c89b9ac8103fa1cb409cf
Jon Díaz <jondi@opendeusto.es>
2021-05-19 10:44:44 +0200

    Separados main y constraint manager

--- ConstraintManager.py
+++ ConstraintManager.py
@@ -3,10 +3,18 @@ class ConstraintManager:
     def __init__(self):
         pass
 
+    def checkConstrains(Route):
+        vehicle = Route.vehicle
 
-    def main():
-        pass
+        if(checkMaxCapacity(vehicle) < vehicle.load):
+            return False
+        
+        if(checkMinLoad(vehicle) > vehicle.load):
+            return False
 
-if __name__ == '__main__':
-    main()
 
+    def checkMaxCapacity(vehicle):
+        return vehicle.capacity
+
+    def checkMinLoad(vehicle):
+        return vehicle.minLoad
---
+++ Main.py
@@ -0,0 +1,5 @@
+from vehicle import Vehicle
+
+vehiculoPrueba = Vehicle(1)
+
+print(vehicle.id)
