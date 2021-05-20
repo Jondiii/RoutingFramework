95d4a71b495a15cc53f762afd32cf1b8565b016d
cdcolano <carlos.delcampo@opendeusto.es>
2021-05-20 09:45:45 +0200

    error en constraint manager

--- ConstraintManager.py
+++ ConstraintManager.py
@@ -12,13 +12,13 @@ class ConstraintManager:
     def checkConstraints(self, Route):
         vehicle = Route.vehicle
         maxCapacity=self.checkMaxCapacity(vehicle)
-        minCapacity=self.checkMinLoad(vehicle)
+        minLoad=self.checkMinLoad(vehicle)
         for i in range (0,len(maxCapacity)):
             if (maxCapacity[i]<vehicle.currentLoad[i]):
                 #if(checkMaxCapacity(vehicle) < vehicle.load):#esto esta mal si gestionamos capacity como bidimensional
                 return False
-        
-            if(minCapacity[i] > vehicle.currentLoad[i]):
+        for i in range (0,len(minLoad)):
+             if(minLoad[i] > vehicle.currentLoad[i]):
                 return False
 
         return True
